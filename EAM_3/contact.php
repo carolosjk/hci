f62218fab2633d46f4172da3baa81e2221e4c466
Adamantia Sofi <adamantiasofi@gmail.com>
2019-12-30 02:13:31 +0200

    error handling in contact form completed

--- EAM_3/contact.php
+++ EAM_3/contact.php
@@ -1,5 +1,7 @@
 <?php
 
+error_reporting(0);
+
 if(!$_POST) exit;
 
 // Email address verification, do not edit.
@@ -9,36 +11,40 @@ function isEmail($email) {
 
 if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
 
-$first_name     = $_POST['first_name'];
-$last_name     = $_POST['last_name'];
-$email    = $_POST['email'];
-$phone   = $_POST['phone'];
-$select_price   = $_POST['select_price'];
-$select_service   = $_POST['select_service'];
-$subject  = $_POST['subject'];
+if(trim($_POST['first_name']) == '') {
+	echo '<div class="error_message">Χρειάζεται να εισαχθεί όνομα.</div>';
+	exit();
+} else if (trim($_POST['last_name']) == '') {
+	echo '<div class="error_message">Χρειάζεται να εισαχθεί επώνυμο.</div>';
+	exit();
+} else if (trim($_POST['phone']) == '') {
+	echo '<div class="error_message">Χρειάζεται να εισαχθεί αριθμός τηλεφώνου.</div>';
+	exit();
+} else if (!is_numeric($_POST['phone'])) {
+	echo '<div class="error_message">Ο αριθμός τηλεφώνου πρέπει να περιλαμβάνει μόνο ψηφία.</div>';
+	exit();
+} else if(trim($_POST['email']) == '') {
+	echo '<div class="error_message">Χρειάζεται να εισαχθεί διεύθυνση email.</div>';
+	exit();
+} else if(!isEmail($_POST['email'])) {
+	echo '<div class="error_message">Μη έγκυρη διεύθυνση.</div>';
+	exit();
+}  
+if(trim($_POST['comments']) == '') {
+	echo '<div class="error_message">Προσθέστε λεπτομέρειες.</div>';
+	exit();
+}
+
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$subject = $_POST['subject'];
 $comments = $_POST['comments'];
-$verify   = $_POST['verify'];
-
-if(trim($first_name) == '') {
-	echo '<div class="error_message">Attention! You must enter your name.</div>';
-	exit();
-}  else if(trim($email) == '') {
-	echo '<div class="error_message">Attention! Please enter a valid email address.</div>';
-	exit();
-} else if(!isEmail($email)) {
-	echo '<div class="error_message">Attention! You have enter an invalid e-mail address, try again.</div>';
-	exit();
-}
-
-if(trim($comments) == '') {
-	echo '<div class="error_message">Attention! Please enter your message.</div>';
-	exit();
-}
 
 if(get_magic_quotes_gpc()) {
 	$comments = stripslashes($comments);
 }
-
 
 // Configuration option.
 // Enter the email address that you want to emails to be sent to.
@@ -60,11 +66,11 @@ $e_subject = 'You\'ve been contacted by ' . $first_name . '.';
 // You can change this if you feel that you need to.
 // Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
 
-$e_body = "You have been contacted by $first_name. $first_name selected service of $select_service, their additional message is as follows. Customer max budge is $select_price, for this project." . PHP_EOL . PHP_EOL;
-$e_content = "\"$comments\"" . PHP_EOL . PHP_EOL;
-$e_reply = "You can contact $first_name via email, $email or via phone $phone";
+// $e_body = "You have been contacted by $first_name. $first_name selected service of $select_service, their additional message is as follows. Customer max budge is $select_price, for this project." . PHP_EOL . PHP_EOL;
+// $e_content = "\"$comments\"" . PHP_EOL . PHP_EOL;
+// $e_reply = "You can contact $first_name via email, $email or via phone $phone";
 
-$msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
+// $msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
 
 $headers = "From: $email" . PHP_EOL;
 $headers .= "Reply-To: $email" . PHP_EOL;
@@ -72,19 +78,14 @@ $headers .= "MIME-Version: 1.0" . PHP_EOL;
 $headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
 $headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
 
-if(mail($address, $e_subject, $msg, $headers)) {
-
+// if(mail($address, $e_subject, $msg, $headers)) {
 	// Email has sent successfully, echo a success page.
-
 	echo "<fieldset>";
 	echo "<div id='success_page'>";
-	echo "<h1>Email Sent Successfully.</h1>";
-	echo "<p>Thank you <strong>$first_name</strong>, your message has been submitted to us.</p>";
+	echo "<h1>Το αίτημά σας στάλθηκε επιτυχώς!</h1>";
+	echo "<p>Ευχαριστούμε <strong>$first_name $last_name</strong>! Θα λάβετε σύντομα απάντηση.</p>";
 	echo "</div>";
 	echo "</fieldset>";
-
-} else {
-
-	echo 'ERROR!';
-
-}
+// } else {
+// 	echo 'ERROR!';
+// }
