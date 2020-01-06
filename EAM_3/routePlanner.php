d7dba144fb52d8627a4ce99295900aac5375cb0c
Karolos Giampouonka-Kanellakos <36304471+carolosjk@users.noreply.github.com>
2020-01-06 17:44:58 +0200

    route planning

--- EAM_3/routePlanner.php
+++ EAM_3/routePlanner.php
@@ -1,13 +1,16 @@
 <?php
     session_start();
-    if ( isset( $_SESSION['user_id']))
-    {
+    $_SESSION['APIkey'] = "AIzaSyBxGFJJ5M8-O_JCjSR-Ib5U_53P4Hpj2uk";
+
+    if (isset($_POST['start']) and isset($_POST['end'])){
+        $start = $_POST['start'];
+        $end = $_POST['end'];
     }
-    $get = (isset($_GET['success'])) ? $_GET['success'] : '';
-    if((!empty($get)) && ($get == 1))
-    {
-        //login successful...
+    else{
+        $start = "1";
+        $end = "1";
     }
+
 ?>
 
 <!DOCTYPE html>
@@ -21,7 +24,7 @@
     <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
  
      <!-- Site Metas -->
-    <title>ΟΑΣΑ</title>  
+    <title>ΟΑΣΑ Προφίλ</title>  
     <meta name="keywords" content="">
     <meta name="description" content="">
     <meta name="author" content="">
@@ -43,10 +46,6 @@
     <!-- Custom CSS -->
     <link rel="stylesheet" href="css/custom.css">
 
-    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/themes/smoothness/jquery-ui.css">
-    <script src="http://code.jquery.com/jquery-1.12.4.js" type="text/javascript"></script>
-    <script src="http://code.jquery.com/ui/1.12.1/jquery-ui.js" type="text/javascript"></script>
-
     <!-- Modernizer for Portfolio -->
     <script src="js/modernizer.js"></script>
 
@@ -54,13 +53,6 @@
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.1/moment-with-locales.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script src="https://cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/d004434a5ff76e7b97c8b07c01f34ca69e635d97/src/js/bootstrap-datetimepicker.js"></script>
-
-    <!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet"> -->
-    <link href="https://cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/d004434a5ff76e7b97c8b07c01f34ca69e635d97/build/css/bootstrap-datetimepicker.css" rel="stylesheet">
 
 </head>
 <body class="realestate_version">
@@ -70,47 +62,58 @@
         <span class="loader"><span class="loader-inner"></span></span>
     </div><!-- end loader -->
     <!-- END LOADER -->
+   
+    <!--HEADER-->
+    <?php include 'utils/uheader.php'; ?>
 
-    <!--ICON BAR-->
-    <div class="icon-bar">
-        <a href="https://www.facebook.com/OASA.GR" class="social-links" target="_blank"><i class="fa fa-facebook global-radius"></i></a> 
-        <a href="https://www.instagram.com/OASA.GR/" class="social-links" target="_blank"><i class="fa fa-instagram global-radius"></i></a> 
-        <a href="https://www.linkedin.com/company/athenspublictransportoasa/about/" class="social-links" target="_blank"><i class="fa fa-linkedin global-radius"></i></a>
-        <a href="https://www.youtube.com/channel/UC0XdkZnOHhRLc3NE9tm4NUQ" class="social-links" target="_blank"><i class="fa fa-youtube global-radius"></i></a> 
+    <div class="all-title-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2>Εύρεση Διαδρομής</h2>
+				</div>
+			</div>
+		</div>
     </div>
-    <!--end of ICON BAR-->
-
-    <!-- HEADER -->
-    <?php include 'utils/header.php'; ?>
-
-    <!--SLIDE SHOW-->
-	<ul class='slideshow'>
-		<li>
-			<span>Slide1</span>
-		</li>
-		<li>		
-			<span>Slide2</span>
-		</li>
-		<li>		
-			<span>Slide3</span>
-		</li>
-		<li>
-			<span>Slide4</span>
-		</li>
-    </ul>
-    <!--end of SLIDE SHOW-->
     
     
-    <!--FIRST SECTION (overlaying slide show)-->
-    <div class="parallax first-section">
+    <!-- SECTION -->
+	<div id="userProfile" class="section wb">
         <div class="container">
-            <div class="row">
-                <div class="col-md-4 wow slideInLeft hidden-xs hidden-sm">
-                    <div class="contact_form">
-                        <h3><i class="fa fa-map-marker grd1 global-radius"></i> ΒΡΕΣ ΔΙΑΔΡΟΜΗ</h3>
+            <!-- Breadcrumbs -->
+			<nav id="breadcrumbs">
+				<ul>
+					<li><a href="index.php"><i class="fa fa-home global-radius fa-lg"></i></a></li>
+					<li>Εύρεση Διαδρομής</li>
+				</ul>
+            </nav>
+            <div class="row" style="float:left;width:200;">
+            <div class="col-md-4 wow slideInLeft hidden-xs hidden-sm" style = "visibility: visible; animation-name: slideInLeft;">
+            <div  style="float:left;width:200" id="routeFind" class="contact_form">
+                        <h3><i class="fa fa-map-marker grd1 global-radius"></i> ΒΡΕΣ NEA ΔΙΑΔΡΟΜΗ</h3>
                         <form action="routePlanner.php" id="contactform1" class="row" name="contactform" method="post">
                             <fieldset class="row-fluid">
                                 <div class="col-lg-12 col-md-6 col-sm-6 col-xs-12">
+                                <script type="text/javascript">
+  function initAutocomplete() {
+    // Create the autocomplete object, restricting the search to geographical
+    // location types.
+    autocomplete = new google.maps.places.Autocomplete(
+        /** @type {!HTMLInputElement} */(document.getElementById('input')),
+        {types: ['geocode']});
+
+    // When the user selects an address from the dropdown, populate the address
+    // fields in the form.
+    autocomplete.addListener('place_changed', fillInAddress);
+  }
+
+  function fillInAddress() {
+    // Get the place details from the autocomplete object.
+    var place = autocomplete.getPlace();
+
+  }
+    </script>
+    <input class="form-control" type="text" id="input" />
                                     <!-- <select name="select_station" id="select_station" class="selectpicker form-control" data-style="btn-white" data-live-search="true"> -->
                                     <select name="start" data-live-search="true" title="Από" data-live-search-placeholder="Επιλέξτε σημείο εκκίνησης" class="form-control">
                                         <option selected disabled hidden value=1>Από</option>
@@ -119,7 +122,9 @@
                                         require('php_utils/db_connect.php');
                                         $sql = mysqli_query($connection, "SELECT * FROM `stations`");
                                         while ($row = $sql->fetch_assoc()){
-                                            echo "<option value=\"" . $row['station'] . "\">" . $row['station'] . "</option>";
+                                            echo "<option ";
+                                            if($row['station'] == $start) echo "selected=\"selected\"";
+                                            echo "  value=\"" . $row['station'] . "\">" . $row['station'] . "</option>";
                                         }
                                         ?>
                                     </select>
@@ -132,7 +137,9 @@
                                         require('php_utils/db_connect.php');
                                         $sql = mysqli_query($connection, "SELECT * FROM `stations`");
                                         while ($row = $sql->fetch_assoc()){
-                                            echo "<option value=\"" . $row['station'] . "\">" . $row['station'] . "</option>";
+                                            echo "<option ";
+                                            if($row['station'] == $end) echo "selected=\"selected\"";
+                                            echo " value=\"" . $row['station'] . "\">" . $row['station'] . "</option>";
                                         }
                                         ?>
                                     </select>
@@ -219,70 +226,31 @@
                             </fieldset>
                         </form>
                     </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-3 col-md-offset-9 wow slideInRight hidden-xs hidden-sm">
-                    <button class="btnn" onclick="window.open('#', '_blank')"> <i class="fa fa-map fa-2x" style="height: 15%; width: 15%; float: right;"></i>Ανακαλύψτε την Αθήνα</button>
                     </div>
+                    
+            <div class="column">
+                <div id="routeMap"  style=" margin-left:10px" >
+                    <?php
+                    if($start != "1" and $end !="1")
+                    echo "<iframe src=\"https://www.google.com/maps/embed/v1/directions".
+                        "?key=".$_SESSION['APIkey'].
+                        "&origin=".$start.
+                        "&destination=".$end.
+                        "&mode=transit&language=el\" width=\"700\" height=\"600\" frameborder=\"0\" style=\"border:0;\" allowfullscreen=\"true\"></iframe>";
+                    ?>
                 </div>
             </div>
-        </div> 
+            </div>
+        </div>
     </div>
-    <!--end of FIRST SECTION-->
 
-    <!--SECOND SECTION-->
-	<div class="about-box">
-		<div class="container">
-			<div class="row">
-                <div class="col-md-4 wow slideInLeft hidden-xs hidden-sm">
-					<div class="item">
-						<div class="single-feature">
-                            <div class="icon"><i class="fa fa-bus fa-5x" style="height: 25%; width: 25%; float:left;"></i>
-                                <h4><a href="buses.php">Λεωφορεία</a></h4>
-                                <h4><a href="#">Τρόλει</a></h4>
-                            </div>  
-                        </div> 
-                    </div>
-                </div>
-                <div class="col-md-4 wow slideInLeft hidden-xs hidden-sm">    
-					<div class="item">
-						<div class="single-feature">
-                            <div class="icon"><i class="fa fa-plane fa-5x" style="height: 25%; width: 25%; float:left;"></i>
-                                <h4><a href="#">Πρόσβαση στο Αεροδρόμιο</a></h4>
-                            </div>      
-                        </div> 
-                    </div>
-                </div>
-                <div class="col-md-4 wow slideInRight hidden-xs hidden-sm">
-                    <div class= "container" style="width: 50%;">
-                        <div class="row" style="gap: 12px;">
-                                <div class="item">
-                                    <div class="single-feature">
-                                        <div class="icon" style="text-align: right;"><i class="fa fa-info-circle fa-3x" style="height: 20%; width: 20%; float:left;"></i>
-                                            <h5><a href="contact.php">Επικοινωνία</a></h5>
-                                        </div>
-                                    </div> 
-                                </div>
-                        </div>
-                        <div class="row" style="padding-top: 12px;">
-                            <div class="item">
-                                <div class="single-feature">
-                                    <div class="icon" style="text-align: right;"><i class="fa fa-question-circle fa-3x" style="height: 20%; width: 20%; float:left;"></i>
-                                        <h5><a href="faq.php">Συχνές Ερωτήσεις</a></h5>
-                                    </div>
-                                </div> 
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>    
     
-    </div>
-    <!--END OF SECOND SECTION-->
+    <!-- end of SECTION -->
 
     <!--FOOTER-->
     <?php include 'utils/footer.php'; ?>
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBxGFJJ5M8-O_JCjSR-Ib5U_53P4Hpj2uk&libraries=places&callback=initAutocomplete" async defer></script>
+
 </body>
 </html>
